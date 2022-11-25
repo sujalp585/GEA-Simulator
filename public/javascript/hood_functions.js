0bd672cc4cd29a551868e36f207a72e6f5e2ede9
sujalp585 <112913128+sujalp585@users.noreply.github.com>
2022-11-24 19:49:02 -0500

    Updated hood setup calls and socket communication

--- public/javascript/hood_functions.js
+++ public/javascript/hood_functions.js
@@ -25,30 +25,7 @@ async function setup(gatewayAccessToken)
     else
     {
       console.log("Socket setup success");
-      alert("Socket setup success! " + response_data.endpoint);
-      socket = new WebSocket(response_data.endpoint);
-
-      socket.onopen = function(e) {
-        alert("Socket is open");
-      };
-      
-      socket.onmessage = function(event) {
-        alert(`Socket message received ${event.data}`);
-      };
-      
-      socket.onclose = function(event) {
-        if (event.wasClean) {
-          alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-        } else {
-          // e.g. server process killed or network down
-          // event.code is usually 1006 in this case
-          alert('[close] Connection died');
-        }
-      };
-      
-      socket.onerror = function(error) {
-        alert(`[error]`);
-      };
+      setupSocket(response_data.endpoint);
     }
   }
   else
@@ -56,142 +33,128 @@ async function setup(gatewayAccessToken)
     alert("HTTP Error (socket setup): " + response.status);
     return;
   }
+
+  await setupGateway();
 }
 
-/*async function setup(gatewayAccessToken)
+function setupSocket(socketEndpoint)
 {
-  //process: gateway sync -> register device presence
+  socket = new WebSocket(socketEndpoint);
 
-  //step 1:
-  const myHeaders = new Headers();
-  myHeaders.append('Host', 'device.mysmarthq.com')
-  myHeaders.append('Authorization', gatewayAccessToken);
-  myHeaders.append('Content-Type', 'application/json');
-
- let data = {
-  "devices": [{
-   "deviceType": "cloud.smarthq.device.hood",
-   "removable": true,
-   "deviceId": "123456789",
-   "services": [
-    {
-     "serviceType": "cloud.smarthq.service.mode",
-     "domainType": "cloud.smarthq.domain.brightness",
-     "supportedCommands": ["cloud.smarthq.command.mode.set"],
-     "serviceId": "1bb051964bd9c5dca58a",
-     "serviceDeviceType": "cloud.smarthq.device.hood.light",
-     "config": {
-      "ordered": true,
-      "supportedModes": [
-       "cloud.smarthq.type.mode.off",
-       "cloud.smarthq.type.mode.dim",
-       "cloud.smarthq.type.mode.high"
-      ]
-     }
-    },
-    {
-     "serviceType": "cloud.smarthq.service.mode",
-     "domainType": "cloud.smarthq.domain.speed",
-     "supportedCommands": ["cloud.smarthq.command.mode.set"],
-     "serviceId": "c8287e53df2909c50fe3",
-     "serviceDeviceType": "cloud.smarthq.device.hood.fan",
-     "config": {
-      "ordered": true,
-      "supportedModes": [
-       "cloud.smarthq.type.mode.off",
-       "cloud.smarthq.type.mode.low",
-       "cloud.smarthq.type.mode.medium",
-       "cloud.smarthq.type.mode.high",
-       "cloud.smarthq.type.mode.boost"
-      ]
-     }
+  socket.onopen = function(e) {
+    //alert("Socket is open at " + socketEndpoint);
+  };
+  
+  socket.onmessage = function(event) {
+    alert("Socket message received " + event.data);
+  };
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      alert('Socket connection closed cleanly, code=${event.code} reason=${event.reason}');
+    } else {
+      alert('Socket Connection died!');
     }
-   ]
-  }],
-  "kind": "gateway#sync",
-  "gateway": {
-   "serial": "SERIAL",
-   "model": "GatewaySimulator",
-   "version": "VERSION",
-   "gatewayId": "rt9e83niwhgp2s2Simulator"
-  }
- };
+  };
+  
+  socket.onerror = function(error) {
+    alert('Socket error!');
+  };
+}
 
-  let response = await fetch('https://device-fld.mysmarthq.com/v2/gateway', 
-  {
-    method: 'POST',
-    headers: myHeaders,
-    body: data
-  });
-
-  if(response.ok)
-  {
-    let response_data = await response.json();
-    console.log(response_data);
-
-    if(response_data.success == "false")
-    {
-      alert("SyncToGateway failed!");
-      return;
-    }
-    else
-    {
-      console.log("SyncToGateway success. Last sync time: " + response_data.syncTime);
-    }
-  }
-  else
-  {
-    alert("HTTP Error (SyncToGateway): " + response.status);
-    return;
+async function setupGateway()
+{
+  if(socket.readyState != WebSocket.OPEN){
+    await new Promise(resolve => setTimeout(resolve, 2000));
   }
 
+  //send "gateway sync" call to socket
+  var payload = {
+    "action": "gateway",
+    "kind": "gateway#sync",
+    "gateway": {
+      "serial": "SERIAL",
+      "model": "GatewaySimulator",
+      "version": "VERSION",
+      "gatewayId": "rt9e83niwhgp2s2Simulator"
+     },
+    "devices": [{
+      "deviceType": "cloud.smarthq.device.hood",
+      "removable": true,
+      "deviceId": "123456789",
+      "services": [
+        {
+         "serviceType": "cloud.smarthq.service.mode",
+         "domainType": "cloud.smarthq.domain.brightness",
+         "supportedCommands": ["cloud.smarthq.command.mode.set"],
+         "serviceId": "1bb051964bd9c5dca58a",
+         "serviceDeviceType": "cloud.smarthq.device.hood.light",
+         "config": {
+          "ordered": true,
+          "supportedModes": [
+           "cloud.smarthq.type.mode.off",
+           "cloud.smarthq.type.mode.dim",
+           "cloud.smarthq.type.mode.high"
+          ]
+         }
+        },
+        {
+         "serviceType": "cloud.smarthq.service.firmware.v1",
+         "domainType": "cloud.smarthq.domain.firmware",
+         "supportedCommands": ["cloud.smarthq.command.firmware.v1.upgrade"],
+         "serviceId": "620df6ba3c1a4b8f1fb5",
+         "serviceDeviceType": "cloud.smarthq.device.wifi",
+         "config": {}
+        },
+        {
+         "serviceType": "cloud.smarthq.service.mode",
+         "domainType": "cloud.smarthq.domain.speed",
+         "supportedCommands": ["cloud.smarthq.command.mode.set"],
+         "serviceId": "c8287e53df2909c50fe3",
+         "serviceDeviceType": "cloud.smarthq.device.hood.fan",
+         "config": {
+          "ordered": true,
+          "supportedModes": [
+           "cloud.smarthq.type.mode.off",
+           "cloud.smarthq.type.mode.low",
+           "cloud.smarthq.type.mode.medium",
+           "cloud.smarthq.type.mode.high",
+           "cloud.smarthq.type.mode.boost"
+          ]
+         }
+        },
+        {
+         "serviceType": "cloud.smarthq.service.firmware.v1",
+         "domainType": "cloud.smarthq.domain.firmware",
+         "supportedCommands": ["cloud.smarthq.command.firmware.v1.upgrade"],
+         "serviceId": "db3b5203c7557975aed9",
+         "serviceDeviceType": "cloud.smarthq.device.appliance",
+         "config": {}
+        }
+       ]
+     }]
+  };
 
-  //step 2:
-  data = {
-    kind: "device#presence",
-    deviceId: "123456789",
-    presence: "ONLINE"
-  }
+  socket.send(payload);
 
-  response = await fetch('https://device-fld.mysmarthq.com/v2/device/presence', 
-  {
-    method: 'POST',
-    headers: myHeaders,
-    body: JSON.stringify(data)
-  });
+  //send "register device presence" to socket
+  payload = {
+    "action": "gateway",
+    "kind": "device#presence",
+    "deviceId": "123456789",
+    "presence": "ONLINE"
+  };
 
-  if(response.ok)
-  {
-    let response_data = await response.json();
-    console.log(response_data);
-
-    if(response_data.success == "false")
-    {
-      alert("RegisterDevice failed!");
-    }
-    else
-    {
-      console.log("RegisterDevice success!");
-    }
-  }
-  else
-  {
-    alert("HTTP Error (RegisterDevice): " + response.status);
-  }
-}*/
+  socket.send(payload);
+}
 
 
 var global_light_state = "cloud.smarthq.type.mode.off";
 var global_fan_state = "cloud.smarthq.type.mode.off";
-var global_delay_active = "false";
+var global_delay_active = false;
 
 async function update_state(button_name)
 {
-  const myHeaders = new Headers();
-  myHeaders.append('Host', 'device.mysmarthq.com')
-  myHeaders.append('Authorization', gatewayAccessToken);
-  myHeaders.append('Content-Type', 'application/json');
-
   switch(button_name)
   {
     case "light_off":
@@ -221,17 +184,18 @@ async function update_state(button_name)
       break;
 
     case "delay_on":
-      global_delay_active = "true";
+      global_delay_active = true;
       break;
     case "delay_off":
-      global_delay_active = "false";
+      global_delay_active = false;
       break;
   }
 
-  let data = {
+  let payload = {
+    action: "gateway",
     kind: "device#services",
     deviceId: "123456789",
-    service: [
+    services: [
       {
         serviceType: "cloud.smarthq.service.mode",
         state: global_light_state,
@@ -252,29 +216,5 @@ async function update_state(button_name)
     ]
   };
 
-  let response = await fetch('https://device-fld.mysmarthq.com/v2/device/state', 
-  {
-    method: 'POST',
-    headers: myHeaders,
-    body: JSON.stringify(data)
-  });
-
-  if(response.ok)
-  {
-    let response_data = await response.json();
-    console.log(response_data);
-
-    if(response_data.success == "false")
-    {
-      alert("Device command was not successful!");
-    }
-    else
-    {
-      console.log("Updated device state successfully!");
-    }
-  }
-  else
-  {
-    alert("HTTP Error (device state update): " + response.status);
-  }
+  socket.send(JSON.stringify(payload)); 
 }
